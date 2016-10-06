5b79fa267c234e54fa8790df8dffae82f8dfe06c
Sebastian Tunstig <sebastian.tunstig@gmail.com>
2016-10-06 14:31:26 +0200

    refarctor shell script using koalaman/shellcheck

--- tldr.sh
+++ tldr.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 tempfile=$(mktemp /tmp/tldr.XXXXX)
 
-tee $tempfile | while read line; do printf "."; done
-printf "\nTLDR; output stored in $tempfile\n"
+tee "$tempfile" | while read -r; do printf "."; done
+printf "\nTLDR; output stored in %s\n" "$tempfile"
+
